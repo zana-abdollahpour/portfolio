3f89e7724d6fb801eda526a2a36f09bd610954dc
zana-abdollahpour <zana.ap4@gmail.com>
2024-04-27 15:29:16 +0330

    add DarkModeButton to Header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,12 @@
 import Logo from "./Logo";
+import DarkModeButton from "./DarkModeButton";
 
 export default function Header() {
   return (
     <header className="container mx-auto flex items-center justify-between bg-red-600 p-2">
       <Logo />
       <div className="flex gap-4">
-        <div>DARKMODEBTN</div>
+        <DarkModeButton />
         <div>MENU</div>
       </div>
     </header>
