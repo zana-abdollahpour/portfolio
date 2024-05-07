04b5ce975b172e263bf3e159433e52850fe58348
zana-abdollahpour <zana.ap4@gmail.com>
2024-05-07 12:52:53 +0330

    add MainTechs to CaseStudy

--- src/components/caseStudies/CaseStudy.tsx
+++ src/components/caseStudies/CaseStudy.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
-import { HiChevronDoubleDown } from "react-icons/hi2";
 
 import Credentials from "./Credentails";
+import MainTechs from "./MainTechs";
 
 export interface ICaseStudy {
   id?: number;
@@ -26,28 +26,16 @@ export default function CaseStudy({
   misc,
   credentials = [],
 }: ICaseStudy) {
-  // const handleExplanationToggle = (e: MouseEventHandler<HTMLDivElement>) => {
-  //   const ExplanationParagraph = e.currentTarget.nextElementSibling;
-  //   const ExplanationIcon = e.currentTarget.lastChild;
-  //   ExplanationParagraph.classList.toggle("pointer-events-auto");
-  //   ExplanationParagraph.classList.toggle("pointer-events-none");
-  //   ExplanationParagraph.classList.toggle("scale-y-0");
-  //   ExplanationParagraph.classList.toggle("opacity-0");
-  //   ExplanationParagraph.classList.toggle("opacity-100");
-  //   ExplanationParagraph.classList.toggle("h-0");
-  //   ExplanationIcon.classList.toggle("-rotate-180");
-  // };
-
   return (
     <div className="flex flex-col gap-8 rounded-lg bg-[var(--project-bg)]">
       <div className="flex flex-col">
-        <h3 className="p-2 text-2xl font-thin tracking-wide">{title}</h3>
+        <h3 className="-mb-2 p-2 text-2xl font-thin tracking-wide">{title}</h3>
         <span className="mb-6 p-2 text-sm">
           {isResponsive ? "Responsive" : "Desktop Only"}
         </span>
         <div className="justify-centerpy-10 mb-8 flex items-center px-8">
           <Image
-            className="w-full rounded-2xl"
+            className="w-full rounded-md"
             src={imagePath}
             width={800}
             height={800}
@@ -57,15 +45,16 @@ export default function CaseStudy({
         <Link
           target="_blank"
           href={link}
-          className="mx-auto w-11/12 max-w-96 rounded-full bg-[var(--project-link)] px-6 py-2 text-center text-xl transition-all hover:scale-95 hover:opacity-85 active:translate-y-1 active:scale-90"
+          className="mx-auto w-10/12 max-w-96 rounded-full bg-[var(--project-link)] px-6 py-2 text-center text-xl transition-all hover:scale-95 hover:opacity-85 active:translate-y-1 active:scale-90 xl:w-11/12"
         >
           Visit
         </Link>
-        <div className="my-8 ml-2 flex flex-col gap-3 border-l pl-4">
+        <div className="my-8 ml-2 flex flex-col gap-2 border-l pl-4">
           <h5 className="text-xl font-bold">Credentials</h5>
           <Credentials credentials={credentials} />
         </div>
       </div>
+
       <div className="w-full p-2">
         <div className="mb-6 rounded-xl border p-2">
           <p className="my-4">{description}</p>
@@ -74,24 +63,9 @@ export default function CaseStudy({
             project and what problem it solves.
           </p>
         </div>
-        <ul>
-          {mainTechs.map((tech, i) => (
-            <li key={i} className="relative mb-8 pl-2">
-              <div
-                // onClick={handleExplanationToggle}
-                className="relative mb-2 flex w-fit origin-left cursor-pointer items-center justify-center gap-2 transition-all hover:scale-105"
-              >
-                <h4 className="font-bold">{tech.at(0)}</h4>
-                <HiChevronDoubleDown className="mt-1 fill-orange-700 transition-all duration-300" />
-              </div>
-              <p className="pointer-events-none relative h-0 origin-top scale-y-0 overflow-hidden border-l pl-2 opacity-0 transition-all duration-300">
-                {tech.at(1)}
-              </p>
-            </li>
-          ))}
-        </ul>
+        <MainTechs mainTechs={mainTechs} />
         <hr className="bg-inherit" />
-        <h4 className="mb-2 ml-2 font-bold">More Dependencies</h4>
+        <h4 className="my-2 ml-2 font-bold">More Dependencies</h4>
         <ul className="my-4">
           {misc.map((item, i) => (
             <li key={i} className="ml-2">
---
+++ src/components/caseStudies/MainTechs.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { type MouseEvent } from "react";
+import { HiChevronDoubleDown } from "react-icons/hi2";
+
+export default function MainTechs({ mainTechs }: { mainTechs: string[][] }) {
+  const handleExplanationToggle = (e: MouseEvent<HTMLDivElement>) => {
+    const paragraph = e.currentTarget.nextSibling as HTMLParagraphElement;
+    const icon = e.currentTarget.lastChild as HTMLElement;
+
+    paragraph.classList.toggle("pointer-events-auto");
+    paragraph.classList.toggle("pointer-events-none");
+    paragraph.classList.toggle("scale-y-0");
+    paragraph.classList.toggle("opacity-0");
+    paragraph.classList.toggle("opacity-100");
+    paragraph.classList.toggle("h-0");
+    icon.classList.toggle("-rotate-180");
+  };
+
+  return (
+    <>
+      <ul>
+        {mainTechs.map((tech, i) => (
+          <li key={i} className="relative mb-8 pl-2">
+            <div
+              onClick={handleExplanationToggle}
+              className="relative mb-2 flex w-fit origin-left cursor-pointer items-center justify-center gap-2 transition-all hover:scale-105"
+            >
+              <h4 className="font-bold">{tech[0]}</h4>
+              <HiChevronDoubleDown className="mt-1 fill-[var(--text-main)] transition-all duration-300" />
+            </div>
+            <p className="pointer-events-none relative h-0 origin-top scale-y-0 overflow-hidden border-l pl-2 opacity-0 transition-all duration-300">
+              {tech[1]}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
